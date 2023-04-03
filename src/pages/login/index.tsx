1e648dfc3de18cab40d401de52e485b8cf916588
Luca George <luca.george44@gmail.com>
2023-04-03 16:19:10 +0300

    login page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,12 +1,17 @@
 import LoginForm from "@/components/LoginComponents/LoginForm";
+import { BackgroundImage } from "@mantine/core";
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import Logo from '../../assets/logoAlb.png';
 
 const index = () => {
-    return <div className="w-full">
-        <h1>test</h1>
-        <LoginForm/>
-    </div>;
+    return (
+        <div className="w-full flex items-center flex-col h-screen justify-center bg-login-pattern bg-cover ">
+            <Image src={Logo} alt="logo" className="phone:max-w-xs"/>
+            <LoginForm />
+        </div>
+    );
 };
 
 export default index;
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,28 @@ module.exports = {
         "./src/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
-        extend: {},
+        extend: {
+            backgroundImage: {
+                "login-pattern":
+                    "url('https://images.unsplash.com/photo-1478760329108-5c3ed9d495a0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80')",
+            },
+            fontFamily: {
+                'sans': 'Helvetica, Arial, sans-serif',
+                'ibm': "Montserrat, sans-serif"
+              }
+        },
+        screens: {
+            'phone': '360px',
+
+            'tablet': '640px',
+            // => @media (min-width: 640px) { ... }
+      
+            'laptop': '1024px',
+            // => @media (min-width: 1024px) { ... }
+      
+            'desktop': '1280px',
+            // => @media (min-width: 1280px) { ... }
+          },
     },
     plugins: [],
 };
